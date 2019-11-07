22e27c8d8f4a8f2981446b4a056ef057e4713b5e
Harry Chung <hc9825@gmail.com>
2019-11-07 14:56:40 -0800

    Updates

---
+++ precache-manifest.af37eb87272c5f73be47219eb44afa60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62a0f1cce4e9310a39a4666e625691a",
+    "url": "/stocktwits-client/index.html"
+  },
+  {
+    "revision": "fb8f908eebb394edbc00",
+    "url": "/stocktwits-client/static/css/main.2d26cf09.chunk.css"
+  },
+  {
+    "revision": "d2ce7cd1fbda7d1f275b",
+    "url": "/stocktwits-client/static/js/2.8fec30bb.chunk.js"
+  },
+  {
+    "revision": "fb8f908eebb394edbc00",
+    "url": "/stocktwits-client/static/js/main.40a11669.chunk.js"
+  },
+  {
+    "revision": "6b1bdc7b70ed39e32f25",
+    "url": "/stocktwits-client/static/js/runtime-main.d3d0f6ea.js"
+  }
+]);
